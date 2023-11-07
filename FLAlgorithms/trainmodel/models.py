499249b2a9a9a987564af55402802499cfbfa62c
jimmylau <jimmylaukh@gmail.com>
2023-11-07 12:45:54 +0800

    Resnet18 only

--- FLAlgorithms/trainmodel/models.py
+++ FLAlgorithms/trainmodel/models.py
@@ -219,14 +219,14 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, channels, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+            layers.append(block(self.in_planes, channels, stride))
+            self.in_planes = channels
         return nn.Sequential(*layers)
 
     def forward(self, x):
