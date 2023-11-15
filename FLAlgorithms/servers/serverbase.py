Update the function to update batch running mean and variance. WHich tends to wrong result in previous analysis

FLAlgorithms/servers/serverbase.py
@@ -121,7 +121,7 @@ class Server:
         assert (self.users is not None and len(self.users) > 0)
 
         # store previous parameters
-        previous_param = copy.deepcopy(list(self.model.parameters()))
+        previous_model = self.model.state_dict()
         global_model = self.model.state_dict()
         for key, data in global_model.items():
             global_model[key]= (torch.zeros_like(data))
@@ -134,11 +134,16 @@ class Server:
         for user in self.selected_users:
             self.add_parameters(global_model, user, user.train_samples / total_train)
             #self.add_parameters(user, 1 / len(self.selected_users))
-        self.model.load_state_dict(global_model)
+        # self.model.load_state_dict(global_model)
 
         # aaggregate avergage model with previous model using parameter beta 
-        for pre_param, param in zip(previous_param, self.model.parameters()):
-            param.data = (1 - self.beta)*pre_param.data + self.beta*param.data
+        # for pre_param, param in zip(previous_param, self.model.parameters()):
+            # param.data = (1 - self.beta)*pre_param.data + self.beta*param.data
+        for key in previous_model:
+            self.model[key] = (1 - self.beta)*previous_model[key] + self.beta*self.model[key]
+            # param.data = (1 - self.beta)*pre_param.data + self.beta*param.data
+        
+        self.model.load_state_dict(global_model)
             
     # Save loss, accurancy to h5 fiel
     def save_results(self, t= None):
@@ -319,3 +324,16 @@ class Server:
         # print("Average Personal Accurancy: ", glob_acc)
         # print("Average Personal Trainning Accurancy: ", train_acc)
         # print("Average Personal Trainning Loss: ",train_loss)
+        
+    def update_user_BN(self):
+        for c in self.users:
+            c.user_upload_BN()
+        print("updated user BN")
+
+    
+    def update_server_BN(self):
+        global_model = self.model.state_dict()
+        for key, data in global_model.items():
+            if(key.endswith("running_var") or key.endswith("running_mean")):
+                global_model[key]= (torch.zeros_like(data))
+        

FLAlgorithms/users/userbase.py
@@ -88,7 +88,6 @@ class User:
         # test_acc = 0
         with torch.no_grad():
             for x, y in self.testloaderfull:
-                print("length: ",len(y))
                 true_label.extend(y.numpy())
                 x, y = x.to(self.device), y.to(self.device)
                 output = self.model(x)
@@ -174,6 +173,15 @@ class User:
     def load_model(self):
         model_path = os.path.join("models", self.dataset)
         self.model = torch.load(os.path.join(model_path, "server" + ".pt"))
+    
+    def user_upload_BN(self):
+        self.model.train()
+        for X, y in self.trainloader:
+            # self.optimizer.zero_grad()
+            X = X.to(self.device)
+            output = self.model(X)
+            # loss = self.loss(output, y)
+            # loss.backward()
     
     @staticmethod
     def model_exists():

analysis.py
@@ -44,27 +44,24 @@ def analyse(dataset, algorithm, model, batch_size, learning_rate, beta, lamda, n
         else: 
             model = DNN(60,20,10).to(device), model
     
-    path = "models/Cifar10_dist_caifarnet/FedAvg_server.pt"
-    model = model[0].to(cpu)
-    assert (os.path.exists(path))
-    model.load_state_dict(torch.load(path))
-    model = model.to(device),model
+    # path = "models/Cifar10_dist_caifarnet/FedAvg_server.pt"
+    # model = model[0].to(cpu)
+    # assert (os.path.exists(path))
+    # model.load_state_dict(torch.load(path))
+    # model = model.to(device),model
     # server.send_parameters()
     # server.evaluate()
 
     if(algorithm == "FedAvg"):
         server = FedAvg(device, dataset, algorithm, model, batch_size, learning_rate, beta, lamda, num_glob_iters, local_iters, optimizer, numusers, 1)
         path = "models/Cifar10_dist_caifarnet/FedAvg_server.pt"
-        # if device == torch.device('cpu'):
-        #     # state_dict =torch.load(path, map_location=device)
-        #     server.model.load_state_dict(torch.load(path), map_location=device)
-        # else:
-        # server.model = server.model.to(cpu)
-        # assert (os.path.exists(path))
+        assert (os.path.exists(path))
         # server.model.load_state_dict(torch.load(path))
-        # server.model =  server.model.to(device)
+        server.model = torch.load(path)
+        server.model = server.model.to(device)
         server.send_parameters()
-        server.evaluate()
+        server.update_user_BN()
+        server.aggregate_parameters()
         
         true_label, predict_label = server.test_and_get_label()
         plot_cm(true_label,predict_label, "FedAvg")
@@ -76,9 +73,12 @@ def analyse(dataset, algorithm, model, batch_size, learning_rate, beta, lamda, n
         
     if(algorithm == "pFedMe"):
         server = pFedMe(device, dataset, algorithm, model, batch_size, learning_rate, beta, lamda, num_glob_iters, local_iters, optimizer, numusers, K, personal_learning_rate, 1)
-        path = "models/Cifar10/pFedMe_server.pt"
+        path = "models/Cifar10_dist_caifarnet/pFedMe_server.pt"
         server.model = torch.load(path)
+        server.model = server.model.to(device)
         server.send_parameters()
+        server.update_user_BN()
+        server.aggregate_parameters()
             # make prediction to global model
         true_label = []
         predict_label = []
@@ -162,9 +162,12 @@ def analyse(dataset, algorithm, model, batch_size, learning_rate, beta, lamda, n
 
     if(algorithm == "PerAvg"):
         server = PerAvg(device, dataset, algorithm, model, batch_size, learning_rate, beta, lamda, num_glob_iters, local_iters, optimizer, numusers, 1)
-        path = "models/Cifar10/PerAvg_server.pt"
+        path = "models/Cifar10_dist_caifarnet/PerAvg_server.pt"
         server.model = torch.load(path)
+        server.model = server.model.to(device)
         server.send_parameters()
+        server.update_user_BN()
+        server.aggregate_parameters()
         # make prediction to global model
         true_label = []
         predict_label = []