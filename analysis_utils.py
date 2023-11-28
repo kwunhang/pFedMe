c9c02067dc48cf5280bc6a636baadd5cef9a3143
jimmylau <jimmylaukh@gmail.com>
2023-11-28 18:45:52 +0800

    util

--- analysis_utils.py
+++ analysis_utils.py
@@ -30,7 +30,7 @@ def plot_cm(true_labels, predict_labels, model_name):
     plt.tight_layout()
     plt.colorbar()
     plt.show()
-    plt.savefig(fname=("cifar_plot/cm_"+model_name))
+    plt.savefig(fname=("ISIC19/cm_"+model_name))
     return plt
 
 def computePRF(true_labels, predicted_labels,model_name):
@@ -62,5 +62,5 @@ def computePRF(true_labels, predicted_labels,model_name):
     ax.legend()
 
     plt.axis([-1, len(label), 0, 1])
-    plt.savefig(fname=("cifar_plot/prf_"+model_name))
+    plt.savefig(fname=("ISIC19/prf_"+model_name))
     plt.show()
