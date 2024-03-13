75023ac24cc473001afab328f84bf61bcae2629a
jimmylau <jimmylaukh@gmail.com>
2024-03-14 02:10:53 +0800

    Add new data augmentation

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -11,8 +11,8 @@ import random
 from torch.utils.data import Dataset, DataLoader
 
 
-# import albumentations as A
-# from albumentations.pytorch.transforms import ToTensorV2
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 
 import os
@@ -373,7 +373,7 @@ def read_user_data(index,data,dataset):
         print("load dataset for ISIC19_raw case")
         train_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_train, y_train)]
         test_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_test, y_test)]
-        train_data = ISIC19Dataset(train_data, transform=train_transforms())
+        train_data = ISIC19Dataset(train_data, transform=new_train_transforms())
         test_data = ISIC19Dataset(test_data, transform=valid_transforms())
         print("print sample")
         print(train_data.get_sample)
@@ -415,7 +415,39 @@ def train_transforms():
     #     A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=255.0),
     #     ToTensorV2(p=1.0),
     # ], p=1.0)
-    
+def new_train_transforms():
+    transforms = A.Compose([
+        A.HorizontalFlip(p=0.5),
+        A.Perspective(scale=(0.05, 0.1), keep_size=True, p=0.1),
+        A.GaussNoise(var_limit=(10.0, 50.0), mean=0, per_channel=True, p=0.1),
+        # group 1
+        A.OneOf([
+            A.Compose([A.MotionBlur(blur_limit=9,p=0.1),],p=1.0),
+            A.Compose([A.MedianBlur(blur_limit=3, p=0.005),],p=1.0),
+            A.Compose([A.Blur(blur_limit=3, p =0.005),],p=1.0),
+        ], p=0.05),
+        # group 2
+        A.OneOf([
+            A.Compose([A.OpticalDistortion(distort_limit=0.5,shift_limit=0.5, p=0.03),],p=1.0),
+            A.Compose([A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.1),],p=1.0),
+            A.Compose([A.PiecewiseAffine(scale=(0.03, 0.05), nb_rows=4, nb_cols=4, p=0.03)],p=1.0),
+        ], p=0.2),
+        # group 3
+        A.OneOf([
+            A.Compose([A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), always_apply=False, p=0.5),],p=1.0),
+            A.Compose([A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), always_apply=False, p=0.5),],p=1.0),
+            A.Compose([A.Emboss(alpha=(0.2, 0.5), strength=(0.2, 0.7), always_apply=False, p=0.5),],p=1.0),
+            A.Compose([A.RandomBrightnessContrast(
+            brightness_limit=0.1, 
+            contrast_limit=0.2, brightness_by_max=True, always_apply=False, p=0.5),],p=1.0),
+        ], p=0.05),
+
+        A.PadIfNeeded(min_height=224, min_width=224),
+        
+        A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=255.0),
+        ToTensorV2(p=1.0),
+    ], p=1.0)
+    return transforms
 
 # only resize, scale [-1, 1] and converting to tensor array[h,w,c] -> tensor[c,h,w]
 def valid_transforms():
