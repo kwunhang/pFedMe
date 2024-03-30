3d7f6eada975f0bebd8eacd65a1958bbbbb02d04
FrankieKwok <frankiecc9@gmail.com>
2024-03-30 22:18:02 +0800

    updated

--- convert_pt_to_csv.py
+++ convert_pt_to_csv.py
@@ -19,7 +19,7 @@ def convert_pt_to_csv(pt_file_path, csv_file_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--analysis_files", nargs='+', default=[""])
-    parser.add_argument("--dataset", default="ISIC2019")
+    parser.add_argument("--dataset", default="ISIC19_raw")
     args = parser.parse_args()
     print("analysis_files       : {}".format(args.analysis_files))      
     
