d6996e3266ec232087b6115ad23cb824913272c6
FrankieKwok <frankiecc9@gmail.com>
2024-03-30 22:25:39 +0800

    debug

--- convert_pt_to_csv.py
+++ convert_pt_to_csv.py
@@ -12,11 +12,13 @@ def convert_pt_to_csv(pt_file_path, csv_file_path):
         for key in header:
             values = train_results[key]
             if values.dim() == 0:  # If values is a scalar
-                values = values.reshape(1, -1)  # Reshape it to a 1D tensor
-            for value in zip(*values):
-                writer.writerow(value)
+                values = [values.item()]  # Convert it to a list
+            else:
+                values = values.tolist()  # Convert tensor to list
+            writer.writerow(values)
     
     return csv_file_path
+
 
 
             
