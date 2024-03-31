43af8452db9c0016f0b747af75de35b7bc6d707a
jimmylau <jimmylaukh@gmail.com>
2024-03-31 20:23:24 +0800

    Debug fully local program

--- ISIC_fully_local_res50.py
+++ ISIC_fully_local_res50.py
@@ -25,12 +25,21 @@ from analysis_utils import plot_function
 
 load_dotenv()
 
+EPOCHS = 150
 learning_rate = 0.001
 batch_size = 128
-save_path = '/ISIC19/fully_res50local_base/'
+save_path = 'ISIC19/fully_res50local_base/'
 checkpoint_path = 'check_point.pth'
-EPOCHS = 150
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device:",device)
+
+base_path = os.getenv('STORAGE_PATH')
+if not(base_path == None or base_path == ""):
+    save_path = os.path.join(base_path, save_path)
+
+print(save_path)
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
 def train_transforms():
     transforms = A.Compose([
@@ -144,12 +153,15 @@ model = resnet50.to(device)
 
 dataset = "ISIC19_raw"
 _ , _ , train_data_tmp, test_data_tmp = read_data(dataset)
-train_data,test_data = [], []
+X_train, y_train, X_test, y_test = [np.array([])]*4
 for _, data in train_data_tmp.items():
-    train_data = train_data + data
+    X_train = np.append(X_train,data['x'])
+    y_train = np.append(y_train, data['y'])
+
 for _, data in test_data_tmp.items():
-    test_data = test_data + data
-X_train, y_train, X_test, y_test = train_data['x'], train_data['y'], test_data['x'], test_data['y']
+    X_test = np.append(X_test,data['x'])
+    y_test = np.append(y_test, data['y'])
+# X_train, y_train, X_test, y_test = train_data['x'], train_data['y'], test_data['x'], test_data['y']
 X_train = torch.Tensor(X_train).view(-1, 3, 224, 224).type(torch.float32)
 y_train = torch.Tensor(y_train).type(torch.int64)
 X_test = torch.Tensor(X_test).view(-1, 3, 224, 224).type(torch.float32)
@@ -163,7 +175,7 @@ train_samples = len(train_data)
 test_samples = len(test_data)
 
 trainloader = DataLoader(train_data, batch_size, shuffle=True, num_workers=2)
-testloader =  DataLoader(test_data, test_samples,num_workers=2)
+testloader =  DataLoader(test_data, batch_size,num_workers=2)
 
 
 criterion = nn.NLLLoss()
@@ -177,13 +189,14 @@ for epoch in range(1, EPOCHS+1):
     time_1 = time.time()
     train_loss, train_accs = [], 0
     model.train()
-    for step, batch in tqdm(enumerate(trainloader, 1), total=len(trainloader)):
+    for step, batch in enumerate(tqdm(trainloader)):
         model.train()
         images, labels = batch
+        images, labels = images.to(device), labels.to(device)
         
         optimizer.zero_grad()
         out = model(images)
-        loss = criterion(out)
+        loss = criterion(out, labels)
         
         loss.backward()
         optimizer.step()
@@ -194,36 +207,47 @@ for epoch in range(1, EPOCHS+1):
         train_accs += accuracy
 
     train_accs /= train_samples
-    
+    cur_train_loss = np.mean(train_loss)
     print('epoch:', epoch, 
-                  '\ttrain loss:', '{:.4f}'.format(np.mean(train_loss)),
+                  '\ttrain loss:', '{:.4f}'.format(np.mean(cur_train_loss)),
                   '\ttrain accuracy:','{:.4f}'.format(train_accs),
                   '\ttime:', '{:.4f}'.format((time.time()-time_1)), 's')
+    rs_train_accs.append(train_accs)
+    rs_train_loss.append(cur_train_loss)
     
     valid_loss, valid_accs = [], 0
     model.eval()
-
-    for step, batch in enumerate(tqdm(testloader)):
-        model.eval()
-        images, label = batch
-        out = model(images)
-        loss = criterion(out)
-        
-        accuracy = (torch.sum(torch.argmax(out, dim=1) == labels)).item()
-        
-        valid_loss.append(loss.item())
-        valid_accs += (accuracy)
+    
+    with torch.no_grad():
+        for step, batch in enumerate(tqdm(testloader)):
+            model.eval()
+            images, labels = batch
+            images, labels = images.to(device), labels.to(device)
+            
+            out = model(images)
+            loss = criterion(out, labels)
+            
+            accuracy = (torch.sum(torch.argmax(out, dim=1) == labels)).item()
+            
+            valid_loss.append(loss.item())
+            valid_accs += (accuracy)
     valid_accs /= train_samples
+    
+    cur_valid_loss = np.mean(valid_loss)
         
     print('epoch:', epoch, '/', EPOCHS,
-          '\tvalid loss:', '{:.4f}'.format(np.mean(valid_loss)),
+          '\tvalid loss:', '{:.4f}'.format(cur_valid_loss),
           '\tvalid accuracy', '{:.4f}'.format(valid_accs))
+    rs_valid_acc.append(valid_accs)
+    rs_valid_loss.append(cur_valid_loss)
+    
     if valid_accs > best_valid_acc:
         torch.save(model.state_dict(), save_path + checkpoint_path)
         print(f'Model weights saved to {save_path}{checkpoint_path}')
         
 # save result of acc and loss
-rs_train_accs, rs_train_loss, rs_valid_acc, rs_valid_loss = [],[],[],[]
+if not os.path.exists(save_path+ 'result/'):
+    os.makedirs(save_path+ 'result/')
 
 with h5py.File(save_path+ 'result/' +'result.h5', 'w') as hf:
     hf.create_dataset('rs_train_accs', data=rs_train_accs)
@@ -235,10 +259,10 @@ with h5py.File(save_path+ 'result/' +'result.h5', 'w') as hf:
 # plot graph
 data = read_test_byClient(dataset, "final_test")
 _ , _ , _, test_data_tmp = read_data(dataset)
-test_data = [], []
+X_test, y_test = [np.array([])]*2
 for _, data in test_data_tmp.items():
-    test_data = test_data + data
-X_test, y_test = test_data['x'], test_data['y']
+    X_test = np.append(X_test,data['x'])
+    y_test = np.append(y_test, data['y'])
 X_test = torch.Tensor(X_test).view(-1, 3, 224, 224).type(torch.float32)
 y_test = torch.Tensor(y_test).type(torch.int64)
 test_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_test, y_test)]
@@ -246,27 +270,29 @@ test_data = ISIC19Dataset(test_data, transform=valid_transforms())
 
 test_samples = len(test_data)
 
-testloader =  DataLoader(test_data, test_samples,num_workers=2)
+testloader =  DataLoader(test_data, batch_size,num_workers=2)
 predict_output = []
 true_label = []
 graph_path = ""
-for step, batch in enumerate(tqdm(testloader)):
-    model.eval()
-    images, label = batch
-    out = model(images)
-    loss = criterion(out)
-    
-    predict = (torch.argmax(out, dim=1) )
-    
-    accuracy = (torch.sum(torch.argmax(out, dim=1) == labels)).item()
-    
-    print("accuracy in validation:", accuracy/len(label))
-    true_label += label
-    predict_output += predict
+model.eval()
+accs = []
+with torch.no_grad():
+    for step, batch in enumerate(tqdm(testloader)):
+        model.eval()
+        images, labels = batch
+        true_label.extend(labels.numpy())
+        images, labels = images.to(device), labels.to(device)
+        
+        out = model(images)
+        loss = criterion(out, labels)
+        
+        predict = (torch.argmax(out, dim=1) )
+        
+        accuracy = (torch.sum(torch.argmax(out, dim=1) == labels)).item()
+
+        predict_output.extend(predict.cpu().numpy())
+        accs.append(accuracy*1.0)
+    print("accuracy in validation:", sum(accs)/len(true_label))
     
 os.environ["SAVE_PLOT_PATH"] = save_path + "graph/"
 plot_function(true_label, predict_output, "full_local")
-
-
-
-
