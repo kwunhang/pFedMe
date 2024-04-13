3b7649f5dc7bac440016a4b263682636cdaa1f02
jimmylau <jimmylaukh@gmail.com>
2024-04-14 03:54:38 +0800

    fix

--- local_seresnext.py
+++ local_seresnext.py
@@ -22,6 +22,8 @@ import os
 from dotenv import load_dotenv
 from tqdm.notebook import tqdm
 from analysis_utils import plot_function
+from utils.transform_utils import ISIC_raw_train_transforms, ISIC_raw_valid_transforms
+from utils.model_utils import ISIC19DatasetRawImage
 import pretrainedmodels
 import ssl
 
@@ -32,8 +34,8 @@ load_dotenv()
 os.environ["TORCH_HOME"] = "/research/d2/fyp23/khlau1/pretrainedmodels/"
 EPOCHS = 150
 learning_rate = 0.0001
-batch_size = 256
-save_path = 'ISIC19/se_resnext_fc/'
+batch_size = 128
+save_path = 'ISIC19/se_resnext_fc_finetune/'
 checkpoint_path = 'check_point.pth'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("device:",device)
@@ -46,99 +48,7 @@ print(save_path)
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-def train_transforms():
-    transforms = A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.Perspective(scale=(0.05, 0.1), keep_size=True, p=0.1),
-        A.GaussNoise(var_limit=(10.0, 50.0), mean=0, per_channel=True, p=0.1),
-        # group 1
-        A.OneOf([
-            A.Compose([A.MotionBlur(blur_limit=9,p=0.1),],p=1.0),
-            A.Compose([A.MedianBlur(blur_limit=3, p=0.005),],p=1.0),
-            A.Compose([A.Blur(blur_limit=3, p =0.005),],p=1.0),
-        ], p=0.05),
-        # group 2
-        A.OneOf([
-            A.Compose([A.OpticalDistortion(distort_limit=0.5,shift_limit=0.5, p=0.03),],p=1.0),
-            A.Compose([A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.1),],p=1.0),
-            A.Compose([A.PiecewiseAffine(scale=(0.03, 0.05), nb_rows=4, nb_cols=4, p=0.03)],p=1.0),
-        ], p=0.2),
-        # group 3
-        A.OneOf([
-            A.Compose([A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), always_apply=False, p=0.5),],p=1.0),
-            A.Compose([A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), always_apply=False, p=0.5),],p=1.0),
-            A.Compose([A.Emboss(alpha=(0.2, 0.5), strength=(0.2, 0.7), always_apply=False, p=0.5),],p=1.0),
-            A.Compose([A.RandomBrightnessContrast(
-            brightness_limit=0.1, 
-            contrast_limit=0.2, brightness_by_max=True, always_apply=False, p=0.5),],p=1.0),
-        ], p=0.05),
 
-        A.PadIfNeeded(min_height=224, min_width=224),
-        
-        A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=255.0),
-        ToTensorV2(p=1.0),
-    ], p=1.0)
-    return transforms
-
-# only resize, scale [-1, 1] and converting to tensor array[h,w,c] -> tensor[c,h,w]
-def valid_transforms():
-    transforms = A.Compose([
-        A.PadIfNeeded(min_height=224, min_width=224),        
-        A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=255.0),
-        ToTensorV2(p=1.0),
-    ], p=1.0)
-    return transforms
-
-
-class ISIC19Dataset(Dataset):
-    """ISIC19 raw dataset in tensor (convert from numpy)"""
-    """Assume the input is just resize to 244 and without normalization"""
-
-    def __init__(self, data, transform=None):
-        """
-        Arguments:
-            data (array): Array of X,Y tuple with tensor data.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.data = data
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
-        x, y = self.data[idx]
-
-        if self.transform:
-            # x = np.transpose(x.numpy(), (1,2,0)) #ToTensorV2 change[h,w,c] -> [c,h,w], revert the change
-            
-            # x = self.transform(x)
-            
-            x = np.array(x)
-            image = {"image": x}
-            image = self.transform(**image)["image"]
-            x=image
-                
-
-        return x,y
-    
-    def get_sample(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
-        x, y = self.data[idx]
-
-        return x,y
-    
-    def collate_fn(self, batch):
-        images, labels = list(zip(*batch))
-        images, labels = [[tensor[None] for tensor in subset] for subset in (images, labels)]
-        images, labels = [torch.cat(subset, dim=0) for subset in (images, labels)]
-        return images, labels
 
 seResNext = pretrainedmodels.__dict__["se_resnext50_32x4d"](num_classes=1000, pretrained='imagenet')
 num_ftrs = seResNext.last_linear.in_features
@@ -153,25 +63,17 @@ seResNext.last_linear = nn.Sequential(
     nn.LogSoftmax(dim=1))
 model = seResNext.to(device)
 
-dataset = "ISIC19_raw"
+dataset = "ISIC19_raw_img_splited"
 _ , _ , train_data_tmp, test_data_tmp = read_data(dataset)
-X_train, y_train, X_test, y_test = [np.array([])]*4
+train_data, test_data = [],[]
 for _, data in train_data_tmp.items():
-    X_train = np.append(X_train,data['x'])
-    y_train = np.append(y_train, data['y'])
-
+    train_data.extend(data)
+    
 for _, data in test_data_tmp.items():
-    X_test = np.append(X_test,data['x'])
-    y_test = np.append(y_test, data['y'])
-# X_train, y_train, X_test, y_test = train_data['x'], train_data['y'], test_data['x'], test_data['y']
-X_train = torch.Tensor(X_train).view(-1, 3, 224, 224).type(torch.float32)
-y_train = torch.Tensor(y_train).type(torch.int64)
-X_test = torch.Tensor(X_test).view(-1, 3, 224, 224).type(torch.float32)
-y_test = torch.Tensor(y_test).type(torch.int64)
-train_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_train, y_train)]
-test_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_test, y_test)]
-train_data = ISIC19Dataset(train_data, transform=train_transforms())
-test_data = ISIC19Dataset(test_data, transform=valid_transforms())
+    test_data.extend(data)
+
+train_data = ISIC19DatasetRawImage(train_data, transform=ISIC_raw_train_transforms())
+test_data = ISIC19DatasetRawImage(test_data, transform=ISIC_raw_valid_transforms())
 
 train_samples = len(train_data)
 test_samples = len(test_data)
@@ -180,6 +82,7 @@ trainloader = DataLoader(train_data, batch_size, shuffle=True, num_workers=2)
 testloader =  DataLoader(test_data, batch_size,num_workers=2)
 
 weighting_loss = []
+y_train = torch.Tensor([y for _,y in train_data]).type(torch.int64)
 for i in range(8):
     weighting_loss.append(train_samples/(torch.sum(y_train==i).item()))
 
@@ -265,16 +168,15 @@ with h5py.File(save_path+ 'result/' +'result.h5', 'w') as hf:
     hf.close()
     
 # plot graph
-data = read_test_byClient(dataset, "final_test")
-_ , _ , _, test_data_tmp = read_data(dataset)
-X_test, y_test = [np.array([])]*2
+model.load_state_dict(torch.load(save_path + checkpoint_path))
+_ , _ , train_data_tmp, test_data_tmp = read_test_byClient(dataset, "final_test")
+
+test_data = []
+    
 for _, data in test_data_tmp.items():
-    X_test = np.append(X_test,data['x'])
-    y_test = np.append(y_test, data['y'])
-X_test = torch.Tensor(X_test).view(-1, 3, 224, 224).type(torch.float32)
-y_test = torch.Tensor(y_test).type(torch.int64)
-test_data = [(transforms.ToPILImage()(x), y) for x, y in zip(X_test, y_test)]
-test_data = ISIC19Dataset(test_data, transform=valid_transforms())
+    test_data.extend(data)
+
+test_data = ISIC19DatasetRawImage(test_data, transform=ISIC_raw_valid_transforms())
 
 test_samples = len(test_data)
 
